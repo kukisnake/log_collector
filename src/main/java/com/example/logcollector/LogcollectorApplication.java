432369ac2610b465426e862ff53e966d2e47bde1
kukisnake <kuklinski.piotr89@gmail.com>
2022-09-02 20:20:07 +0200

    Start of the exercise

---
+++ src/main/java/com/example/logcollector/LogcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.logcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogcollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/logcollector/LogcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
